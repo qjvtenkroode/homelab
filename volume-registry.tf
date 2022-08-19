73c1d68bedc6ba51b583199ba57258a43c913866
Quincey ten Kroode <qjv.tenkroode@gmail.com>
2022-08-19 19:37:15 +0200

    volume: prometheus added

--- volume-registry.tf
+++ volume-registry.tf
@@ -22,3 +22,28 @@ resource "nomad_volume" "registry" {
     share              = "/mnt/homelab/csi/vols/registry"
   }
 }
+
+resource "nomad_volume" "prometheus" {
+  type                  = "csi"
+  plugin_id             = "truenas"
+  volume_id             = "prometheus"
+  name                  = "prometheus"
+  external_id           = "prometheus"
+  deregister_on_destroy = true
+
+  capability {
+    access_mode           = "single-node-writer"
+    attachment_mode       = "file-system"
+  }
+
+  mount_options {
+    fs_type = "nfs"
+  }
+
+  context = {
+    node_attach_driver = "nfs"
+    provisioner_driver = "freenas-nfs"
+    server             = "truenas.qkroode.nl"
+    share              = "/mnt/homelab/csi/vols/prometheus"
+  }
+}
