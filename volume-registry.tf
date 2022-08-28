2bcd30b102548ab46eb3411d4b4fe82d88a6e33c
Quincey ten Kroode <qjv.tenkroode@gmail.com>
2022-08-28 19:53:31 +0200

    volume: vaultwarden added

--- volume-registry.tf
+++ volume-registry.tf
@@ -47,3 +47,28 @@ resource "nomad_volume" "prometheus" {
     share              = "/mnt/homelab/csi/vols/prometheus"
   }
 }
+
+resource "nomad_volume" "vaultwarden" {
+  type                  = "csi"
+  plugin_id             = "truenas"
+  volume_id             = "vaultwarden"
+  name                  = "vaultwarden"
+  external_id           = "vaultwarden"
+  deregister_on_destroy = true
+
+  capability {
+    access_mode           = "single-node-writer"
+    attachment_mode       = "file-system"
+  }
+
+  mount_options {
+    fs_type = "nfs"
+  }
+
+  context = {
+    node_attach_driver = "nfs"
+    provisioner_driver = "freenas-nfs"
+    server             = "truenas.qkroode.nl"
+    share              = "/mnt/homelab/csi/vols/vaultwarden"
+  }
+}
