0ba893646d77f79e5e221448a1dc07c929abf04f
Quincey ten Kroode <qjv.tenkroode@gmail.com>
2023-11-15 09:47:22 +0100

    terraform: change job.tf to reflect repository restructuring

--- terraform/jobs.tf
+++ terraform/jobs.tf
@@ -1,49 +1,49 @@
 resource "nomad_job" "plugin-nfs-controller" {
-    jobspec = file("${path.module}/../homelab-nomad/csi/plugin-nfs-controller.nomad")
+    jobspec = file("${path.module}/../nomad/csi/plugin-nfs-controller.nomad")
 }
 
 resource "nomad_job" "plugin-nfs-node" {
-    jobspec = file("${path.module}/../homelab-nomad/csi/plugin-nfs-node.nomad")
+    jobspec = file("${path.module}/../nomad/csi/plugin-nfs-node.nomad")
 }
 
 resource "nomad_job" "fabio" {
-    jobspec = file("${path.module}/../homelab-nomad/fabio.nomad")
+    jobspec = file("${path.module}/../nomad/fabio.nomad")
 }
 
 resource "nomad_job" "registry" {
-    jobspec = file("${path.module}/../homelab-nomad/registry.nomad")
+    jobspec = file("${path.module}/../nomad/registry.nomad")
     depends_on = [nomad_volume.registry]
 }
 
 resource "nomad_job" "cribl" {
-    jobspec = file("${path.module}/../homelab-nomad/cribl.nomad")
+    jobspec = file("${path.module}/../nomad/cribl.nomad")
     depends_on = [nomad_volume.cribl]
 }
 
 resource "nomad_job" "qkroode" {
-    jobspec = file("${path.module}/../homelab-nomad/qkroode.nomad")
+    jobspec = file("${path.module}/../nomad/qkroode.nomad")
     depends_on = [nomad_job.registry]
 }
 
 resource "nomad_job" "vaultwarden" {
-    jobspec = file("${path.module}/../homelab-nomad/vaultwarden.nomad")
+    jobspec = file("${path.module}/../nomad/vaultwarden.nomad")
     depends_on = [nomad_volume.vaultwarden]
 }
 
 resource "nomad_job" "homeassistant" {
-    jobspec = file("${path.module}/../homelab-nomad/homeassistant.nomad")
+    jobspec = file("${path.module}/../nomad/homeassistant.nomad")
     depends_on = [nomad_volume.homeassistant]
 }
 
 #resource "nomad_job" "prometheus" {
-#    jobspec = file("${path.module}/../homelab-nomad/monitoring/prometheus.nomad")
+#    jobspec = file("${path.module}/../nomad/monitoring/prometheus.nomad")
 #    depends_on = [nomad_volume.prometheus]
 #}
 
 resource "nomad_job" "photocatalog" {
-    jobspec = file("${path.module}/../homelab-nomad/photoprism.nomad")
+    jobspec = file("${path.module}/../nomad/photoprism.nomad")
 }
 
 resource "nomad_job" "mqtt" {
-    jobspec = file("${path.module}/../homelab-nomad/mqtt.nomad")
+    jobspec = file("${path.module}/../nomad/mqtt.nomad")
 }
